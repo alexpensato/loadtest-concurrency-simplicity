b41e9ef5a4c20dbdda3c23c6b57445cf095d5f6a
alexpensato <20325394+alexpensato@users.noreply.github.com>
2020-07-16 00:39:04 +0200

    Rename Response Bean

--- src/main/java/org/pensatocode/loadtester/bean/CountResponseBean.java
+++ src/main/java/org/pensatocode/loadtester/bean/CountResponseBean.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CountersBean {
+public class CountResponseBean {
     private CounterBean counts;
     private CounterBean atomicCounters;
 }
